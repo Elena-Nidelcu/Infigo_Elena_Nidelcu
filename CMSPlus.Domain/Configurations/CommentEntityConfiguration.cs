af5b8e67c776c7eb58ac8430a60bc0e22dc5b761
enidelcu <enidelcu@gmail.com>
2025-01-17 21:13:38 +0200

    create CommentEntityConfiguration

---
+++ CMSPlus.Domain/Configurations/CommentEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CMSPlus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CMSPlus.Domain.Configurations;
+
+public class CommentEntityConfiguration : IEntityTypeConfiguration<CommentEntity>
+{
+    public void Configure(EntityTypeBuilder<CommentEntity> builder)
+    {
+        builder.ToTable("Comments");
+
+        // Configure properties
+        builder.Property(x => x.FullName).IsRequired();
+        builder.Property(x => x.Content).IsRequired();
+
+        // Configure relationships
+        builder.HasOne(x => x.Topic)
+            .WithMany(x => x.Comments)
+            .HasForeignKey(x => x.TopicId);
+    }
+}
--- CMSPlus.Domain/Entities/CommentEntity.cs
+++ CMSPlus.Domain/Entities/CommentEntity.cs
@@ -1,13 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CMSPlus.Domain.Entities;
 
 public class CommentEntity : BaseEntity
 {
-    public string FullName { get; set; } = null!; // Name of the person adding the comment
-    public string Content { get; set; } = null!; // The actual comment
-
-    // Foreign key to TopicEntity
+    public string FullName { get; set; } = null!;
+    public string Content { get; set; } = null!;
     public int TopicId { get; set; }
     public TopicEntity Topic { get; set; } = null!;
 }
