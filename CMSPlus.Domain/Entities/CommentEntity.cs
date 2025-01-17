44ac14ffb4433cb2f5b5184633d22ade604f7b45
enidelcu <enidelcu@gmail.com>
2025-01-17 14:41:22 +0200

    add CommentEntity class

---
+++ CMSPlus.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMSPlus.Domain.Entities;
+
+public class CommentEntity : BaseEntity
+{
+    public string FullName { get; set; } = null!; // Name of the person adding the comment
+    public string Content { get; set; } = null!; // The actual comment
+
+    // Foreign key to TopicEntity
+    public int TopicId { get; set; }
+    public TopicEntity Topic { get; set; } = null!;
+}
--- CMSPlus.Domain/Persistance/ApplicationDbContext.cs
+++ CMSPlus.Domain/Persistance/ApplicationDbContext.cs
@@ -1,28 +1,37 @@
 ﻿using System.Reflection;
 using CMSPlus.Domain.Configurations;
 using CMSPlus.Domain.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace CMSPlus.Domain.Persistance;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    // Constructor for dependency injection
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
 
+    // DbSet properties to map entities to database tables
+    public DbSet<TopicEntity> Topics { get; set; } = null!;
+    public DbSet<CommentEntity> Comments { get; set; } = null!;
+
+    // Optional: Configuring the database options
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
     }
 
+    // Configuring model relationships and constraints
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        // Apply configurations for entities
         builder.ApplyConfiguration(new TopicEntityConfiguration());
+        builder.ApplyConfiguration(new CommentEntityConfiguration()); // Add this if you have a CommentEntityConfiguration
+
+        // Call the base method to ensure Identity tables are configured
         base.OnModelCreating(builder);
     }
 }
